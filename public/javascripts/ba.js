264799eb883a572853ae0f45da3f2e6654ed1036
XLandMine <645007605@qq.com>
2016-03-12 09:59:54 +0800

    采用框架重写ajax
    舍弃原生ajax，改用jQuery的ajax

--- public/javascripts/ba.js
+++ public/javascripts/ba.js
@@ -1,7 +1,8 @@
 var arr = [];
 var tm = null;
-var i = 0;        //将要播放的帧数
-var fps = 62;   //一帧的播放速度
+var i = 0;       	//将要播放的帧数
+var fps = 62;  		//一帧的播放速度
+var cacheIndex = 0; //定义资源标识符
 var current = null;
 var previous = null;
 var mtk = $("#mtk");
@@ -9,70 +10,18 @@ var cacheFlag = false;
 var startFlag = false;
 
 
-function addInfo($obj,txt){
-	$obj.append("<li>" + txt + "</li>");
-	if($obj.children().length > 5){
-		$obj.children().first().remove();
-	}
-}
+
 
 //页面加载完就获取资源
 $(function(){
-	var xhr = new XMLHttpRequest();
-	// var baTxtStr = null;
-	xhr.i = 0;//绑定index标识
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState == 4){
-			if((xhr.status>=200&&xhr.status<300)||xhr.status == 304){
-				
-				//将获取到的内容通过“,”切割成字符串
-				arr = arr.concat(xhr.responseText.split(","));
-				mtk.children()[0].innerHTML += "<br>资源加载完成"+xhr.i+"0%";
-				addInfo($("#info > ul"),"资源加载完成"+xhr.i+"0%");
-				mtk.children("button").animate({"opacity":"1"},1000);
-				
-				if(xhr.i == 10){
-					mtk.children()[0].innerHTML += "<br>资源全部加载成功";
-					addInfo($("#info > ul"),"资源全部加载成功");
-					cacheFlag = true;
-					//显示开始按钮
-					//将获取到的内容
-					
-					//播放时间为220秒 用时间/长度得出一帧播放的时间
-					// fps = 220*1000/arr.length;
 
-					//释放内存
-					return;
-				}
-				//判断此时播放是否因为缓存而暂停
-				if(startFlag){
-					current = new Date();
-	    			previous = new Date();
-	    			//继续播放视频和字符串
-	    			$("video")[0].play();
-					start();
-					startFlag = false;
-				}
-				xhr.open("get","badapple?num="+xhr.i,true);
-				xhr.i++;
-				xhr.send(null);
-			}else{
-				mtk.children()[0].innerHTML += "<br>资源加载失败,失败码为:"+xhr.status;
-				addInfo($("#info > ul"),"资源加载失败,失败码为:"+xhr.status);
-			}
-		}
-	}
-	mtk.children()[0].innerHTML += "<br>资源加载中....";
-	mtk.children()[0].innerHTML += "<br>由于带有视频，缓存时间略长，请安心等待";
-	mtk.children()[0].innerHTML += "<br>缓存10%即可观看";
 	addInfo($("#info > ul"),"资源加载中....");
 	addInfo($("#info > ul"),"由于带有视频，缓存时间略长，请安心等待");
 	addInfo($("#info > ul"),"缓存10%即可观看");
+	//用ajax的get方法访问服务器获取数据
 	//发送url到后台获取数据，由于数据量大，所以进行分10段去获取
-	//通过num标识要获取第几段数据
-	xhr.open("get","badapple?num="+xhr.i,true);
-	xhr.i++;
-	xhr.send(null);
+	//通过num标识要获取第几段数据,cacheIndex为当前获取的资源标识符
+	$.get("badapple",{num:cacheIndex},ajaxGetBa);
 	
 
 	$("#btn").click(function(){
@@ -122,18 +71,52 @@ function start(){
 		}
 		// console.log("dt----"+dt);
 		tm = setTimeout("start()",time);
-	}else if(!cacheFlag){
+	}else if(cacheIndex < 9){
+		//资源标识符小于9说明资源还未全部获取完毕
 		addInfo($("#info > ul"),"缓冲中。。。");
 		startFlag = true;
 		$("video")[0].pause();
 	}else{
+		addInfo($("#info > ul"),"播放完成");
+		//三秒后拉上mtk
 		setTimeout(function(){
-			mtk.children()[0].innerHTML += "<br>播放完成";
-			addInfo($("#info > ul"),"播放完成");
 			mtk.animate({"top":0});
-		},1000)
+		},3000)
 	}
 
 	i++;
 
+}
+function ajaxGetBa(data){
+	//将获取到的内容通过“,”切割成字符串并合并到arr中
+	arr = arr.concat(data.split(","));
+	
+	addInfo($("#info > ul"),"资源加载完成"+(cacheIndex+1)+"0%");
+	mtk.children("button").animate({"opacity":"1"},1000);
+
+	//当资源标识符为9时表明资源全部获取完毕，停止资源获取
+	if(cacheIndex == 9){
+		addInfo($("#info > ul"),"资源全部加载成功");
+		return;
+	}
+	//判断此时播放是否因为缓存而暂停
+	if(startFlag){
+		current = new Date();
+		previous = new Date();
+		//继续播放视频和字符串
+		$("video")[0].play();
+		start();
+		startFlag = false;
+	}
+	// 自增资源标识符
+	cacheIndex++;
+	// 再次获取数据
+	$.get("badapple",{num:cacheIndex},ajaxGetBa);
+}
+function addInfo($obj,txt){
+	mtk.children()[0].innerHTML += "<br>" + txt;
+	$obj.append("<li>" + txt + "</li>");
+	if($obj.children().length > 5){
+		$obj.children().first().remove();
+	}
 }
