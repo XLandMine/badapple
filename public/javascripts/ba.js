bb6d648e92b8f57fe47873f62277101c61ef28bc
xuanchao <1213211789@qq.com>
2016-12-30 10:04:44 +0800

    fix bug

--- public/javascripts/ba.js
+++ public/javascripts/ba.js
@@ -1,10 +1,14 @@
-function BadApple () {
+function BadApple ($screen,$mask,$info,video) {
 	this.content = [];
 	this.timer = null;
 	this.index = 0;   		//将要播放的帧数
 	this.fps = 62;			//一帧的播放速度
 	this.cacheIndex = 0;	//资源标识符
-	this.startFlag = false;
+	this.stopFlag = false;
+	this.$screen = $screen;
+	this.$mask = $mask;
+	this.$info = $info;
+	this.video = video;
 }
 var fn = BadApple.prototype;
 
@@ -28,23 +32,23 @@ fn.start = function () {
 				console.time("fps");
 
 				waiteTime -= this.fps;
-				//将数组显示到txt控件上，并使i自增
-				$("#txt").val(this.content[i++]);
+				//将数组显示到屏幕上，并使i自增
+				this.$screen.val(this.content[this.index++]);
 			}
 		}else if(this.cacheIndex < 9){
 			//资源标识符小于9说明资源还未全部获取完毕
-			this.addInfo($("#info > ul"),"缓冲中。。。");
+			this.addLog("缓冲中。。。");
 			//设置播放flag为true  表示当前视频并未播放完毕，仅仅是暂停了
-			this.startFlag = true;
-			$("video")[0].pause();
+			this.stopFlag = true;
+			this.video.pause();
 			clearInterval(this.timer);
 		}else{
-			this.addInfo($("#info > ul"),"播放完成");
+			this.addLog("播放完成");
 			//清除定时器
 			clearInterval(this.timer);
 			//三秒后拉上mtk
 			setTimeout(function(){
-				$("#mtk").animate({"top":0});
+				this.$mask.animate({"top":0});
 			},3000)
 		}
 	}.bind(this) ,16);
@@ -54,20 +58,20 @@ fn.getContent = function(data) {
 	//将获取到的内容通过“,”切割成字符串并合并到this.content中
 	this.content = this.content.concat(data.split(","));
 
-	this.addInfo($("#info > ul"),"资源加载完成"+(this.cacheIndex+1)+"0%");
-	$("#mtk").children("button").css("display","block").animate({"opacity":"1"},1000);
+	this.addLog("资源加载完成"+(this.cacheIndex+1)+"0%");
+	this.$mask.children("button").css("display","block").animate({"opacity":"1"},1000);
 
 	//当资源标识符为9时表明资源全部获取完毕，停止资源获取
 	if(this.cacheIndex == 9){
-		this.addInfo($("#info > ul"),"资源全部加载成功");
+		this.addLog("资源全部加载成功");
 		return;
 	}
 	//判断此时播放是否因为缓存而暂停
-	if(this.startFlag){
+	if(this.stopFlag){
 		//继续播放视频和字符串
-		$("video")[0].play();
+		this.video.play();
 		this.start();
-		this.startFlag = false;
+		this.stopFlag = false;
 	}
 	// 自增资源标识符
 	this.cacheIndex++;
@@ -77,11 +81,11 @@ fn.getContent = function(data) {
 	}.bind(this));
 }
 
-fn.addInfo = function addInfo($obj,txt){
-	$("#mtk").children()[0].innerHTML += "<br>" + txt;
-	$obj.append("<li>" + txt + "</li>");
-	if($obj.children().length > 5){
-		$obj.children().first().remove();
+fn.addLog = function addLog(txt){
+	this.$mask.find(".log")[0].innerHTML += "<br>" + txt;
+	this.$info.append("<li>" + txt + "</li>");
+	if(this.$info.children().length > 5){
+		this.$info.children().first().remove();
 	}
 }
 
@@ -89,10 +93,10 @@ fn.addInfo = function addInfo($obj,txt){
 //页面加载完就获取资源
 $(function(){
 
-	var ba = new BadApple();
-	ba.addInfo($("#info > ul"),"资源加载中....");
-	ba.addInfo($("#info > ul"),"由于带有视频，缓存时间略长，请安心等待");
-	ba.addInfo($("#info > ul"),"缓存10%即可观看");
+	var ba = new BadApple($("#txt"),$("#mtk"),$("#info ul"),$("video")[0]);
+	ba.addLog("资源加载中....");
+	ba.addLog("由于带有视频，缓存时间略长，请安心等待");
+	ba.addLog("缓存10%即可观看");
 	//用ajax的get方法访问服务器获取数据
 	//发送url到后台获取数据，由于数据量大，所以进行分10段去获取
 	//通过num标识要获取第几段数据,cacheIndex为当前获取的资源标识符
@@ -102,14 +106,13 @@ $(function(){
 
 	$("#btn").click(function(){
 		var clientHeight = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight|| 2000;
-		$("#mtk").animate({
+		ba.$mask.animate({
 			"top":-clientHeight
 		})
 	    //播放字符动画
 		ba.start();
 		//显示播放器并播放mp4文件
-		$("video").css("opacity","1");
-		$("video")[0].play();
+		$(ba.video).show();
+		ba.video.play();
 	});
 });
-
